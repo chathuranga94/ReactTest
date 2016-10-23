110df7aa64455244db310e3fbfd7a8b91ec20aab
chathuranga94 <chathuranga94@live.com>
2016-10-23 22:33:13 +0530

    intializer

--- React LearnCode/src/js/client.js
+++ React LearnCode/src/js/client.js
@@ -2,9 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Layout extends React.Component{
+
+	constructor(){
+		super();
+		this.name = "Name";
+	}
+
+	getValue(val){
+		return "Value is " + val;
+	}
+
 	render(){
 		return(
-			<h1>It Works</h1>
+			<h1>It Works {this.name} {this.getValue(5)}</h1>
 		);
 	}
 }
