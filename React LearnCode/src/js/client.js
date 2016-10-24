db3e4cc258a2029596cf43fda757a1d625750369
chathuranga94 <chathuranga94@live.com>
2016-10-24 06:47:59 +0530

    multiple js files for components

--- React LearnCode/src/js/client.js
+++ React LearnCode/src/js/client.js
@@ -1,23 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Layout extends React.Component{
+import Layout from "./components/Layout";
 
-	constructor(){
-		super();
-		this.name = "Name";
-	}
-
-	getValue(val){
-		return "Value is " + val;
-	}
-
-	render(){
-		return(
-			<h1>It Works {this.name} {this.getValue(5)}</h1>
-		);
-	}
-}
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
---
+++ React LearnCode/src/js/components/Header/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default class Title extends React.Component{
+
+	render(){
+		return(
+			<h3>HEADER TITLE</h3>
+		);
+	}
+}
