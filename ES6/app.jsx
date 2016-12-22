7f079d95b3c1c6a1ecd10d43695b5971405f27e8
Udara Bibile <chathuranga94@live.com>
2016-12-22 10:05:00 +0530

    organize repo

--- ES6/app.jsx
+++
@@ -1,8 +0,0 @@
-import HelloWorld from './hello-world';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-    <HelloWorld phrase="ES6"/>,
-    document.querySelector('.root')
-);
