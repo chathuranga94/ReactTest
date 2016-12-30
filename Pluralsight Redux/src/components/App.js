376447ff80a6b829cf1ce250a7e7fc0560f7a5da
Udara Bibile <chathuranga94@live.com>
2016-12-30 15:59:52 +0530

    Pluralsight courses added

--- Pluralsight Redux/src/components/App.js
+++ Pluralsight Redux/src/components/App.js
@@ -1,12 +1,13 @@
 // This component handles the app
 // template which other pages use
 import React, {PropTypes} from 'react';
+import Header from './common/Header';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <p>Header here...</p>
+        <Header></Header>
         {this.props.children} 
       </div>
     );
